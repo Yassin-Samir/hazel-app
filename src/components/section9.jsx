0cc889ffeffd596bc1afefdc68505fb8b53357f1
yassin samir <yassinsam3@gmail.com>
2023-11-19 17:18:24 +0200

    Refactor client information rendering logic in section9.jsx component
    The code changes in section9.jsx component involve refactoring the
    client information rendering logic. Previously, the information was
    stored in an array, and the selected client's information was updated
    based on the mouseover event. The refactoring includes renaming the
    state variables, removing unnecessary code, and simplifying the logic
    for updating the selected client's information.
    
    The updated code now uses the useState hook to manage the client
    information rendering. The state variables have been renamed for better
    clarity. The onMouseOver event now updates the Paragraph state variable
    with the selected client's description and the ClientName state variable
    with the selected client's name. The rendering of client images has also
    been simplified by directly setting the opacity style based on the
    selected client.
    
    This refactoring improves the readability and maintainability of the
    section9 component by using clearer variable names and removing
    redundant code.

--- src/components/section9.jsx
+++ src/components/section9.jsx
@@ -6,80 +6,65 @@ import third3 from "../assets/profile/t3.jpeg";
 import fourth4 from "../assets/profile/t4.jpeg";
 import fifth5 from "../assets/profile/t5.jpeg";
 import sixth6 from "../assets/profile/t6.jpeg";
+const arr = [
+  {
+    src: first1,
+    p: "Dynamically whiteboard timely models through bricks-and-clicks e-services. Holisticly predominate resource maximizing users through backend leadership skills. Continually mesh team building strategic theme areas without standards compliant users.",
+    head: "JACK MOE",
+  },
+  {
+    src: second2,
+    p: "Intrinsicly embrace top-line core competencies with real-time metrics. Conveniently reinvent functionalized collaboration. Authoritatively pontificate enabled alignments whereas market positioning",
+    head: "MARIA DOE",
+  },
+  {
+    src: third3,
+    p: "Credibly envisioneer enterprise-wide content before resource maximizing leadership skills. Progressively visualize professional value via distributed value. Efficiently restore adaptive bandwidth through best-of-breed plug-and-play ROI and excellent platforms",
+    head: "SONI MARVEL",
+  },
+  {
+    src: fourth4,
+    p: "Compellingly e-enable distributed deliverables whereas customer directed vortals. Objectively pontificate leveraged collaboration and idea-sharing without bleeding-edge resources. Professionally administrate business web services without future-proof catalysts for change. Energistically engage diverse vortals and prospective methods of empowerment. Competently pontificate diverse",
+    head: "JOTAS DOE",
+  },
+  {
+    src: fifth5,
+    p: "Enthusiastically syndicate inexpensive services and 24/7 bandwidth. Phosfluorescently develop cooperative content vis-a-vis best-of-breed alignments. Competently innovate orthogonal e-tailers via interactive core competencies. Uniquely implement transparent",
+    head: "VAL DRACA",
+  },
+  {
+    src: sixth6,
+    p: "Rapidiously integrate multimedia based resources whereas low-risk high-yield technologies. Proactively innovate market positioning products without B2B products. Progressively recaptiualize",
+    head: "MARIA LUCAS",
+  },
+];
 function section9() {
-  const arr = [
-    {
-      src: first1,
-      p: "Dynamically whiteboard timely models through bricks-and-clicks e-services. Holisticly predominate resource maximizing users through backend leadership skills. Continually mesh team building strategic theme areas without standards compliant users.",
-      ref: useRef(null),
-      head: "JACK MOE",
-    },
-    {
-      src: second2,
-      p: "Intrinsicly embrace top-line core competencies with real-time metrics. Conveniently reinvent functionalized collaboration. Authoritatively pontificate enabled alignments whereas market positioning",
-      ref: useRef(null),
-      head: "MARIA DOE",
-    },
-    {
-      src: third3,
-      p: "Credibly envisioneer enterprise-wide content before resource maximizing leadership skills. Progressively visualize professional value via distributed value. Efficiently restore adaptive bandwidth through best-of-breed plug-and-play ROI and excellent platforms",
-      ref: useRef(null),
-      head: "SONI MARVEL",
-    },
-    {
-      src: fourth4,
-      p: "Compellingly e-enable distributed deliverables whereas customer directed vortals. Objectively pontificate leveraged collaboration and idea-sharing without bleeding-edge resources. Professionally administrate business web services without future-proof catalysts for change. Energistically engage diverse vortals and prospective methods of empowerment. Competently pontificate diverse",
-      ref: useRef(null),
-      head: "JOTAS DOE",
-    },
-    {
-      src: fifth5,
-      p: "Enthusiastically syndicate inexpensive services and 24/7 bandwidth. Phosfluorescently develop cooperative content vis-a-vis best-of-breed alignments. Competently innovate orthogonal e-tailers via interactive core competencies. Uniquely implement transparent",
-      ref: useRef(null),
-      head: "VAL DRACA",
-    },
-    {
-      src: sixth6,
-      p: "Rapidiously integrate multimedia based resources whereas low-risk high-yield technologies. Proactively innovate market positioning products without B2B products. Progressively recaptiualize",
-      ref: useRef(null),
-      head: "MARIA LUCAS",
-    },
-  ];
-  const [p, setp] = useState(
+  const [Paragraph, setParagraph] = useState(
     "Dynamically whiteboard timely models through bricks-and-clicks e-services. Holisticly predominate resource maximizing users through backend leadership skills. Continually mesh team building strategic theme areas without standards compliant users."
   );
-  const [h, seth] = useState("JACK MOE");
-  const [st, setst] = useState(true);
+  const [ClientName, setClientName] = useState("JACK MOE");
   return (
     <div className="section9">
       <p>OUR CLIENTS LOVE US</p>
       <div>
         {arr.map((i, ind) => (
           <img
-            ref={i.ref}
             className="circle"
             src={i.src}
             loading="lazy"
             key={ind}
-            onMouseOver={() => {
-              setp(i.p);
-              setst(false);
-              document
-                .querySelectorAll(".circle")
-                .forEach((i) => (i.style.opacity = "0.5"));
-              i.ref.current.style.opacity = "1";
-              seth(i.head);
+            style={{
+              opacity: ClientName === i.head ? 1 : 0.5,
             }}
-            onMouseLeave={() => {
-              !st
-                ? (i.ref.current.style.opacity = "1")
-                : (i.ref.current.style.opacity = "0.5");
+            onMouseOver={() => {
+              setParagraph(i.p);
+              setClientName(i.head);
             }}
           />
         ))}
-        <p>{p}</p>
+        <p>{Paragraph}</p>
       </div>
-      <p className="head12">{h}</p>
+      <p className="head12">{ClientName}</p>
     </div>
   );
 }
